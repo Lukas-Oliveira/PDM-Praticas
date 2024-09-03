7c8effb8a6b402d37eea5f6aaa396ab83e13568e
Lucas Oliveira <lucas.g.oliveira1998@outlook.com>
2024-09-03 11:26:56 -0300

    Prática 04 - Parte 02 - Refatoração e Permissões

--- app/src/main/java/com/weatherapp/MainActivity.kt
+++ app/src/main/java/com/weatherapp/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.weatherapp
 
+import android.Manifest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -24,23 +27,37 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.weatherapp.ui.CityDialog
 import com.weatherapp.ui.HomePage
 import com.weatherapp.ui.MainViewModel
 import com.weatherapp.ui.nav.BottomNavBar
+import com.weatherapp.ui.nav.BottomNavItem
 import com.weatherapp.ui.nav.MainNavHost
 import com.weatherapp.ui.theme.WeatherAppTheme
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+        val viewModel: MainViewModel by viewModels()
+
         setContent {
 
             val navController = rememberNavController()
-            val viewModel: MainViewModel by viewModels()
             var showDialog by remember { mutableStateOf(value = false) }
+
+            val context = LocalContext.current
+            val currentRoute = navController.currentBackStackEntryAsState()
+            val showButton = currentRoute.value?.destination?.route != BottomNavItem.MapPage.route
+
+            val launcher = rememberLauncherForActivityResult(
+                contract = ActivityResultContracts.RequestPermission(),
+                onResult = {}
+            )
 
             WeatherAppTheme {
 
@@ -72,14 +89,17 @@ class MainActivity : ComponentActivity() {
                         BottomNavBar(navController = navController)
                     },
                     floatingActionButton = {
-                        FloatingActionButton(onClick = { showDialog = true }) {
-                            Icon(Icons.Default.Add, contentDescription = "Adicionar")
+                        if (showButton){
+                            FloatingActionButton(onClick = { showDialog = true }) {
+                                Icon(Icons.Default.Add, contentDescription = "Adicionar")
+                            }
                         }
                     }
                 ) {
                     innerPadding ->
                         Box(modifier = Modifier.padding(innerPadding)) {
-                            MainNavHost(navController = navController, viewModel = viewModel)
+                            launcher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+                            MainNavHost(navController = navController, viewModel = viewModel, context = context)
                         }
                 }
             }
