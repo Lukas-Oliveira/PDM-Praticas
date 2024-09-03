741993d17e268c13a572896ab0d7927888ac49e7
Lucas Oliveira <lucas.g.oliveira1998@outlook.com>
2024-09-03 11:49:15 -0300

    Prática 04 - Parte 03 - Adicionando e Testando o Mapa

--- app/src/main/java/com/weatherapp/ui/MapPage.kt
+++ app/src/main/java/com/weatherapp/ui/MapPage.kt
@@ -2,23 +2,18 @@ package com.weatherapp.ui
 
 import android.app.Activity
 import android.content.Context
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
-import com.weatherapp.R
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true)
@@ -30,19 +25,46 @@ fun MapPage(
 ) {
     var activity = LocalContext.current as? Activity
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(colorResource(id = R.color.teal_200))
-            .wrapContentSize(Alignment.Center)
+    val recife     = LatLng(-8.05, -34.9)
+    val caruaru    = LatLng(-8.27, -35.98)
+    val joaoPessoa = LatLng(-7.12, -34.84)
+
+    val camPosState = rememberCameraPositionState()
+
+    GoogleMap(
+        modifier = Modifier.fillMaxSize(),
+        onMapClick = { viewModel.add("Nova Cidade", location = it) },
+        cameraPositionState = camPosState
     ) {
-        Text(
-            text = "Mapa",
-            fontWeight = FontWeight.Bold,
-            color = Color.White,
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            textAlign = TextAlign.Center,
-            fontSize = 20.sp
+
+        viewModel.cities.forEach {
+            if (it.location != null) {
+                Marker(
+                    state = MarkerState(position = it.location!!),
+                    title = it.name,
+                    snippet = "${it.location}"
+                )
+            }
+        }
+
+        Marker(
+            state = MarkerState(position = recife),
+            title = "Recife",
+            snippet = "Marcador em Recife",
+            icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)
+        )
+        Marker(
+            state = MarkerState(position = caruaru),
+            title = "Caruaru",
+            snippet = "Marcador em Caruaru",
+            icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)
+        )
+        Marker(
+            state = MarkerState(position = joaoPessoa),
+            title = "João Pessoa",
+            snippet = "Marcador em João Pessoa",
+            icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)
         )
     }
+
 }
